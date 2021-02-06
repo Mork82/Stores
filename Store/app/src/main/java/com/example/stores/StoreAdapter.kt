7159d791eda979de8ccb7a6500addf224dc92664
josco <joscorcon@gmail.com>
2021-02-06 11:28:34 +0100

    Recycler view implementado

---
+++ Store/app/src/main/java/com/example/stores/StoreAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.stores
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.stores.databinding.ItemStoreBinding
+
+class StoreAdapter(private var stores: MutableList<Store>, private var listener: OnClickListener) :
+    RecyclerView.Adapter<StoreAdapter.ViewHolder>() {
+
+    private lateinit var mContetx: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        mContetx = parent.context
+
+        val view = LayoutInflater.from(mContetx).inflate(R.layout.item_store, parent, false)
+
+        return ViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val store = stores.get(position)
+
+        with(holder) {
+
+            binding.tvName.text = store.name
+
+        }
+    }
+
+    override fun getItemCount(): Int = stores.size
+
+    fun add(store: Store) {
+        stores.add(store)
+        notifyDataSetChanged()
+
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val binding = ItemStoreBinding.bind(view)
+
+        fun setListener(store: Store) {
+            binding.root.setOnClickListener { listener.onClick(store) }
+        }
+    }
+}
